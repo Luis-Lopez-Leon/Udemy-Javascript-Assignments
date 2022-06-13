1a522423a46f985c26c13dc46dc323f2184989c3
Luis Lopez <andestrious@gmail.com>
2022-06-13 16:13:37 -0500

    fundamentals 1 and coding challenge

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -1,7 +1,43 @@
+//Lecture: Values and Variables
+
 let country = 'United States';
 let continent = 'North America';
-let population = '333,807,743 ';
+let population = 333807743;
 
 console.log(country);
 console.log(continent);
 console.log(population);
+
+//Lecture: Data types
+
+const isIsland = false;
+let language;
+
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
+
+language = 'English';
+//isIsland = true;
+
+//divide population by half
+let halfPopulation = population / 2;
+console.log(halfPopulation);
+
+//add 1 to population
+population++;
+console.log(population);
+
+//my country pop. higher than Finland pop?
+let populationFinland = 6000000;
+console.log(population > populationFinland);//returns true
+
+//my country pop. less than 33 million?
+console.log(population < 33000000);
+
+let description = country + ' is in ' + continent + ', and its ' + population + ' million people speak ' + language;
+
+console.log(description);
+
+
--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,3 +1,4 @@
+/*
 
 let js = 'amazing';
 
@@ -31,3 +32,117 @@ let job1 = "Programmer";
 let job2 = "Teacher";
 
 console.log(myFirstJob);
+
+let javaScriptIsFun = true;
+console.log(javaScriptIsFun);
+
+console.log(typeof true);
+console.log(typeof javaScriptIsFun);
+console.log(typeof 23);
+console.log(typeof "Jonas");
+
+//let declares new variables
+
+//dynamic typing:
+//redefine variable javaScriptIsFun to string
+javaScriptIsFun = "YES!";
+
+//undfine datatype
+let year;
+console.log(year);
+console.log(typeof year);
+
+//bug in javascript. Says that null is an object instead of null datatype
+console.log(typeof null);
+
+
+
+//reassign or mutate the variable
+let age = 30;
+age = 31;
+
+
+//constant variables cannot be changed
+const birthYear = 1992;
+//birthYear = 1991;
+
+//const job;//gives error
+
+//Good pratice to have very little mutations or reassignments
+
+//var is old way to declare variables
+//avoid using var
+var job = "programmer";
+job = "teacher";
+
+//let is block scoped
+//var is function scoped
+
+//possible to declare without using let/const
+//BAD IDEA! as it creates it in global scope instead of the current scope its declared at
+lastName = "Schmedtmann";
+console.log(lastName);
+
+
+
+// OPERATORS
+const NOW = 2037;
+const ageJonas = NOW - 1991;
+const ageSarah = NOW - 2018;
+console.log(ageJonas, ageSarah);
+
+// 2 ** 3 <- means 2 to the power of 3 (2^3)
+//commas separate operations
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+
+const firstName = "Jonas";
+const lastName = "Schmedtmann";
+
+//concatanate strings with + operator
+console.log(firstName + ' ' + lastName);
+
+// = assigment operator
+let x = 10 + 5;//result: 15
+x += 10; // x = x + 10; result: 25
+x *= 4;// x = x * 4; result: 100
+x++; //x = x + 1; result 101
+x--; //x = x - 1; 
+x--; // result 99
+console.log(x);
+
+//Comparison operators
+// <, >, >=, <=
+
+console.log(ageJonas > ageSarah);//retunrs true if correct
+console.log(ageSarah >= 18);//returns true if correct
+
+//can store results
+const isFullAge = ageSarah >= 18;
+
+console.log(now - 1991 > now - 2018);
+
+
+
+const NOW = 2037;
+const ageJonas = NOW - 1991;
+const ageSarah = NOW - 2018;
+
+console.log(NOW - 1991 > NOW - 2018);
+
+//Javascript operator precedence
+
+//mathermatical operators have higher precedence than comparison operators
+
+//some have precedence left-to-right or right-to-left
+
+//console.log(25 - 10 -5);
+
+let x, y;
+x = y = 25 - 10 - 5;//minus operators compute first so answer is 10 (compute from left-to-right). Then assign operators compute after, but from right-to-left. So y becomes 10 first, then x becomes 10 second
+console.log(x, y);
+
+//using parenthesis operators as it has highest precedence to correctly calculate averageAge called 'grouping'
+const averageAge = (ageJonas + ageSarah) / 2;
+console.log(ageJonas, ageSarah, averageAge);
+
+*/
