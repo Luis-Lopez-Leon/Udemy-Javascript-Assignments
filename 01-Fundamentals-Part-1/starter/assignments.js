bf675863f2c3b7e93917663b8cdf6df22fd08de9
Luis Lopez <103616373+Luis-Lopez-Leon@users.noreply.github.com>
2022-06-13 09:59:58 -0500

    Add files via upload

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,7 @@
+let country = 'United Staes';
+let continent = 'North America';
+let population = '333,807,743 ';
+
+console.log(country);
+console.log(continent);
+console.log(population);
---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,33 @@
+
+let js = 'amazing';
+
+console.log(40 + 8 + 23 - 10);
+
+console.log("Jonas");
+console.log(23);
+
+let firstName = "Matilda";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let jonas_matilda = "JM";
+//can use $ or _ to overwrite and use reserved variable names
+let $function = 27;
+console.log(jonas_matilda);
+console.log($function);
+
+let person = "Jonas";
+
+//All Uppercase variables for constants
+let PI = 3.1415;
+
+//descriptive variables - USE THESE!
+let myFirstJob = "Coder";
+let myCurrentJob = "Teacher";
+
+//undescriptive variables
+let job1 = "Programmer";
+let job2 = "Teacher";
+
+console.log(myFirstJob);
